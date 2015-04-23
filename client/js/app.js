875c9b974ada17a10f1b3344663918a7679b059b
Nick Tomlin <nick.tomlin@gmail.com>
2015-04-22 19:50:24 -0500

    Add a simplified first step

--- client/js/app.js
+++ client/js/app.js
@@ -1,60 +1,68 @@
 'use strict';
 
-require('angular');
-global.debug = require('debug');
-global.debug.enable('*');
-var tilApp = angular.module('til', []);
+var angular = require('angular');
+var assign = require('object-assign');
+var EventEmitter = require('events').EventEmitter;
+var Dispatcher = require('Flux').Dispatcher;
 
-tilApp.directive('createTil', require('./components/create-til'));
-tilApp.directive('tilList', require('./components/til-list'));
+var dispatcher = new Dispatcher();
+var app = angular.module('til', []);
 
-tilApp.service('clientActions', require('./actions/client-actions'));
-tilApp.service('serverActions', require('./actions/server-actions'));
-tilApp.service('TilStore', require('./stores/til-store'));
-tilApp.service('CommentStore', require('./stores/comment-store'));
-tilApp.service('UserStore', require('./stores/user-store'));
-tilApp.service('AuthenticationStore', require('./stores/authentication-store'));
+app.controller('index', function ($scope, tilStore, ClientActions) {
+  $scope.tilInput = '';
+  $scope.tils = [];
 
-tilApp.controller('index', require('./pages/index'));
-
-// !! handwaving
-// we preload the application with a user
-// our stores must be injected to attach listeners, so we include them here
-tilApp.run(function (TilStore, UserStore, CommentStore, AuthenticationStore, serverActions, clientActions) {
-  if (global.test) { return; }
-  serverActions.addUser({
-    id: 1,
-    displayName: 'Nick Tomlin'
+  tilStore.addChangeListener(function () {
+    console.log('Controller::Change')
+    $scope.tils = $scope.tils.concat(tilStore.getAll());
   });
 
-  serverActions.addUser({
-    id: 2,
-    displayName: 'Ember Bob'
-  });
-
-  serverActions.receiveTil({
-    userId: 1,
-    text: 'Angular and Flux work together',
-    clientId: 1234
-  });
-
-  serverActions.receiveTil({
-    userId: 2,
-    text: 'It\'s all in your mind',
-    clientId: 5678
-  });
-
-  clientActions.addComment({
-    tilId: TilStore.get()[0].clientId,
-    userId: 2,
-    text: 'But what do you think about ember?'
-  });
-
-  clientActions.addComment({
-    tilId: TilStore.get()[1].clientId,
-    userId: 1,
-    text: 'Deep.'
-  });
+  $scope.addTil = function () {
+    console.log('View::addTil');
+    ClientActions.addTil($scope.tilInput);
+    $scope.tilInput = '';
+  };
 });
 
-module.exports = tilApp;
+app.service('tilStore', function () {
+  var _counter = 0;
+  var _tils = {};
+
+  var tilStore = assign({}, EventEmitter.prototype, {
+    addChangeListener: function (callback) {
+      this.on('change', callback);
+    },
+    getAll: function () {
+      return Object.keys(_tils).map(function (key) {
+        return _tils[key];
+      });
+    }
+  });
+
+  tilStore.dispatchToken = dispatcher.register(function (payload) {
+    switch (payload.type) {
+      case 'ADD_TIL':
+        console.log('Store::handler');
+        _tils[_counter] = {
+          text: payload.text,
+          id: _counter
+        };
+        tilStore.emit('change');
+      break;
+      default:
+         // ¯\_(ツ)_/¯
+    }
+  });
+
+  return tilStore;
+});
+
+app.service('ClientActions', function () {
+  this.addTil = function (text) {
+    console.log('ClientActions::addTil');
+    dispatcher.dispatch({
+      type: 'ADD_TIL',
+      text: text
+    });
+  };
+});
