3015aa51e9ea9e61fa533c1cf6f1aa1e61c0516c
Nick Tomlin <nick.tomlin@gmail.com>
2015-05-02 13:16:26 -0500

    add-actions

--- client/js/app.js
+++ client/js/app.js
@@ -5,3 +5,30 @@ var assign = require('object-assign');
 var uuid = require('uuid').v1;
 var EventEmitter = require('events').EventEmitter;
 var Dispatcher = require('Flux').Dispatcher;
+
+var tilApp = angular.module('til', []);
+var dispatcher = new Dispatcher();
+
+tilApp.controller('index', function ($scope, tilClientActions) {
+  $scope.tils = [{text: 'This is dynamic'}];
+  $scope.tilInput = '';
+  $scope.addTil = function () {
+    console.log('tilController::AddTil', $scope.tilInput);
+    tilClientActions.addTil($scope.tilInput);
+  }
+});
+
+tilApp.service('tilClientActions', function () {
+  this.addTil = function (text) {
+    console.log('tilClientActions', text);
+    dispatcher.dispatch({
+      type: 'ADD_TIL',
+      data: {
+        til: {
+          text: text,
+          clientId: uuid()
+        }
+      }
+    });
+  }
+});
