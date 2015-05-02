0c918a82080fe683a3af7928d0a5331033d6235f
Nick Tomlin <nick.tomlin@gmail.com>
2015-05-02 14:02:21 -0500

    Complete step-one

--- client/js/app.js
+++ client/js/app.js
@@ -9,18 +9,24 @@ var Dispatcher = require('Flux').Dispatcher;
 var tilApp = angular.module('til', []);
 var dispatcher = new Dispatcher();
 
-tilApp.controller('index', function ($scope, tilClientActions) {
-  $scope.tils = [{text: 'This is dynamic'}];
+tilApp.controller('index', function ($scope, clientActionCreators, tilStore) {
+  $scope.tils = [];
   $scope.tilInput = '';
+
+  tilStore.addChangeListener(function () {
+    $scope.tils = tilStore.getAll();
+  });
+
   $scope.addTil = function () {
-    console.log('tilController::AddTil', $scope.tilInput);
-    tilClientActions.addTil($scope.tilInput);
-  }
+    console.log('controller::AddTil', $scope.tilInput);
+    clientActionCreators.addTil($scope.tilInput);
+    $scope.tilInput = '';
+  };
 });
 
-tilApp.service('tilClientActions', function () {
+tilApp.service('clientActionCreators', function () {
   this.addTil = function (text) {
-    console.log('tilClientActions', text);
+    console.log('clientActionCreators::AddTil', text);
     dispatcher.dispatch({
       type: 'ADD_TIL',
       data: {
@@ -31,4 +37,34 @@ tilApp.service('tilClientActions', function () {
       }
     });
   }
+});
+
+tilApp.service('tilStore', function () {
+  var _tils = {};
+
+  var tilStore = assign({}, EventEmitter.prototype, {
+    addChangeListener: function (callback) {
+      this.on('change', callback);
+    },
+    getAll: function () {
+      return Object.keys(_tils).map(function (key) {
+        return _tils[key];
+      });
+    }
+  });
+
+  dispatcher.register(function (payload) {
+    switch (payload.type) {
+      case 'ADD_TIL':
+        console.log('tilStore::ADD_TIL', payload.data);
+        _tils[payload.data.til.clientId] = payload.data.til;
+        tilStore.emit('change');
+      break;
+
+      default:
+        // ¯\_(ツ)_/¯
+    }
+  });
+
+  return tilStore;
 });
